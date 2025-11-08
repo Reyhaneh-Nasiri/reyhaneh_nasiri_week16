8eb0c866bd31efa6a1dc7d6edd3208fe00a87c47
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-11-08 04:54:08 +0330

    feat(city-input): save the searched value

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -1,9 +1,17 @@
-import styles from "./CityInput.module.css"
+import { useState } from "react";
+import styles from "./CityInput.module.css";
 const CityInput = () => {
+  const [search, setSearch] = useState("");
+  console.log(search);
   return (
     <>
       <div className={styles.citySearch}>
-        <input type="text" className={styles.citySearch__input} />
+        <input
+          type="text"
+          className={styles.citySearch__input}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <p className={styles.citySearch__hint}>New York</p>
       </div>
     </>
